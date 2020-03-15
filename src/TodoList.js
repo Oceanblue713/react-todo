14b705ec40b4635081aea53a895399ab8a573511
Anri Liy <amedamachip713@gmail.com>
2020-03-15 13:24:41 -0700

    Add ref function

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,12 +1,26 @@
 import React, { Component } from "react";
 
 class TodoList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      items: []
+    }
+
+    this.addItem = this.addItem.bind(this);
+  }
+
+  addItem(e) {
+
+  }
   render() {
     return(
       <div className = "todoListMain">
         <div className="header">
-          <form>
-            <input placeholder="enter task"></input>
+          <form onSubmit={this.addItem}>
+            <input ref={(a) => this._inputElement = a }
+                    placeholder="enter task"></input>
             <button type="submit">Add</button>
           </form>
         </div>
